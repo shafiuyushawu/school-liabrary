5450621cfc32028a1d92f048403d175266f3b8f6
Shafiu Ibn Yushawu <yishafiu@gmail.com>
2023-05-16 20:08:16 +0000

    Removed the keyword `super`, added `nameable` as attr_accessor in base_deraotor

--- base_decorator.rb
+++ base_decorator.rb
@@ -1,10 +1,13 @@
 require_relative 'nameable'
 
 class BaseDecorator < Nameable
+  attr_accessor :nameable
+
+  # rubocop:disable Lint/MissingSuper
   def initialize(nameable)
-    super
     @nameable = nameable
   end
+  # rubocop:enable Lint/MissingSuper
 
   def correct_name
     @nameable.correct_name
--- person.rb
+++ person.rb
@@ -1,13 +1,14 @@
 require_relative 'nameable'
 
 class Person < Nameable
+  # rubocop:disable Lint/MissingSuper
   def initialize(age, name: 'Unknown', parent_permission: true)
-    super()
     @id = Random.rand(1..1000)
     @name = name
     @age = age
     @parent_permission = parent_permission
   end
+  # rubocop:enable Lint/MissingSuper
 
   attr_reader :id
   attr_accessor :name, :age
