83bc00819575170f8914eae7cde25c36b04d92ea
Shafiu Ibn Yushawu <yishafiu@gmail.com>
2023-05-17 20:48:40 +0000

    Added classroom, book and rental classes and created association between them

--- student.rb
+++ student.rb
@@ -2,11 +2,17 @@ require_relative 'person'
 
 class Student < Person
   def initialize(id, age, classroom, parent_permission: true, name: 'Unknown')
-    super(id, age, parent_permission, name)
-    @classroom = classroom
+    super(age, name: name, parent_permission: parent_permission)
+    @id = id
+    self.classroom = classroom
   end
 
   def play_hooky
     '¯\\(ツ)/¯'
+  end
+
+  def classroom=(classroom)
+    @classroom = classroom
+    @classroom.students << self unless @classroom.students.include?(self)
   end
 end
